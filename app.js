b887c766603af5b7da641bd97ca31bf1f26ef883
Isak La Fleur <isak.engdahl@gmail.com>
2017-11-14 09:16:34 +0100

    small fixes.

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var cors = require("cors");
 
-var index = require('./routes/index');
+var index = require("./routes/index");
 
 var app = express();
 
+// Enable All CORS Requests
+app.use(cors());
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', index);
+app.use("/", index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,19 @@
 function getCoinDeskInfo(startDate, endDate, currency) {
-  $.ajax({
+  axios
+    .get(
+      `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`
+    )
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+
+  /* $.ajax({
     url: `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`,
-    method: 'GET',
-    success (response) {
-      console.log(response);
+    method: "GET",
+    success(response) {
+      // console.log(response);
       const dataobj = JSON.parse(response);
-      console.log(dataobj);
-      console.log(dataobj.bpi);
+      // console.log(dataobj);
+      // console.log(dataobj.bpi);
 
       const data1 = [];
       const data2 = [];
@@ -18,46 +25,51 @@ function getCoinDeskInfo(startDate, endDate, currency) {
         data1.push(property);
         data2.push(dataobj.bpi[property]);
       }
-      console.log(data1);
-      console.log(data2);
+      // console.log(data1);
+      // console.log(data2);
 
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: data1,
-        datasets: [{
-            label: '# of Votes',
-            data: data2,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255,99,132,1)',
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: data1,
+          datasets: [
+            {
+              label: "# of Votes",
+              data: data2,
+              backgroundColor: "rgba(255, 99, 132, 0.2)",
+              borderColor: "rgba(255,99,132,1)",
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
                 ticks: {
-                    beginAtZero:true
+                  beginAtZero: true
                 }
-            }]
+              }
+            ]
+          }
         }
-    }
-});
-
+      });
     },
-    error (err) {
+    error(err) {
       console.log(err);
-    },
-  });
+    }
+  }); */
 }
 
-
-
-$('#getdata').on('click', () => {
-  const sDate = $('#startDate').val();
-  const eDate = $('#endDate').val();
-  const currency =  $('#curr option:selected').text();
-  console.log(currency);
+const getData = document.getElementById("getdata");
+getData.addEventListener("click", () => {
+  const sDate = document.getElementById("startDate").value;
+  const eDate = document.getElementById("endDate").value;
+  const currId = document.getElementById("curr");
+  const currency = currId.options[currId.selectedIndex].text;
+  console.log("currency: ", currency);
+  console.log("sDate: ", sDate);
+  console.log("eDate: ", eDate);
   getCoinDeskInfo(sDate, eDate, currency);
 });
